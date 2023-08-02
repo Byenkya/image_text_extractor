7cacbfd5385ca06beb6e94cfc9e059d7cbaddbfe
Byenkya <byenkyajoshua1997@gmail.com>
2023-08-02 13:27:34 +0300

    added requirements.txt file

--- text_extractor/routes.py
+++ text_extractor/routes.py
@@ -166,14 +166,6 @@ class CaptureImageView(MethodView):
         return render_template('capture.html', form=form)
 
 
-def save_image_on_server(image_data, username):
-    # Remove the "data:image/png;base64," prefix from the base64 data
-    image_data = image_data.split(',')[1]
-
-    # Save the image in a "static/uploads" folder with the username as the filename
-    with open(f'static/uploads/{username}.png', 'wb') as f:
-        f.write(base64.b64decode(image_data))
-
 class DeleteImageView(MethodView):
     decorators = [login_required]
     
--- text_extractor/utils.py
+++ text_extractor/utils.py
@@ -11,3 +11,11 @@ def delete_image_file(filename):
 
     except Exception as e:
         raise e
+    
+def save_image_on_server(image_data, username):
+    # Remove the "data:image/png;base64," prefix from the base64 data
+    image_data = image_data.split(',')[1]
+
+    # Save the image in a "static/uploads" folder with the username as the filename
+    with open(f'static/uploads/{username}.png', 'wb') as f:
+        f.write(base64.b64decode(image_data))
